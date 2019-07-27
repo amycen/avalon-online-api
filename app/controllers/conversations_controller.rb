e869a5390a64d99b310a63413384291fda058dd6
Amy Cen <cenamy@gmail.com>
2019-07-27 14:25:26 -0400

    added convo and msg controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,36 @@
 class ConversationsController < ApplicationController
+
+    def index
+        conversations = Conversation.all
+        render json: conversations
+    end
+
+    def show
+        conversation = Conversation.find(params[:id])
+        if conversation
+            render json: conversation
+        else
+            render json: {errors: "No Conversation Found"}
+        end
+    end
+
+    def create
+        game = Game.find(conversation_params[:game_id])
+        conversation = Conversation.new({game_id: game.id})
+        if game && conversation.save
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(
+                ConversationSerializer.new(conversation)
+            ).serializable_hash
+            ActionCable.server.broadcast 'conversations_channel', serialized_data
+            head :ok
+        else
+            ActionCable.server.broadcast 'conversations_channel', 'Cannot Load Chat'
+        end
+    end
+
+    private
+
+    def conversation_params
+        params.require(:conversation).permit(:game_id)
+    end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+
+    def create
+        conversation = Conversation.find(message_params[:conversation_id])
+        message = Message.new(message_params.merge!(user_id: request.headers["Authorization"].to_i))
+        if conversation && message.save
+            serialized_data = ActiveModelSerializers::Adapter::Json.new(
+                MessageSerializer.new(message)
+            ).serializable_hash
+            MessagesChannel.broadcast_to conversation, serialized_data
+            head :ok
+        #else
+        #    MessagesChannel.broadcast_to conversation, "Unable to send message"
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:text, :conversation_id)
+    end
 end
