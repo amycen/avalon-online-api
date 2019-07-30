2db920b0309d124eba553707f6bddbb37bf01d9f
Amy Cen <cenamy@gmail.com>
2019-07-29 20:58:19 -0400

    roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,6 +1,31 @@
 class RolesController < ApplicationController
 
     def get_roles
-        puts "hi"
+        roles = []
+        roles << Role.find_by({abbreviation: "MER"})
+        roles << Role.find_by({abbreviation: "ASS"})
+        remaining_players = params[:playerNum].to_i - 2
+        if (remaining_players >= 3)
+            roles << Role.find_by({abbreviation: "LS1"})
+            roles << Role.find_by({abbreviation: "LS2"})
+            roles << Role.find_by({abbreviation: "MM1"})
+        end
+        if (remaining_players >= 4)
+            roles << Role.find_by({abbreviation: "LS3"})
+        end
+        if (remaining_players >= 5)
+            roles << Role.find_by({abbreviation: "MM2"})
+        end
+        if (remaining_players >= 6)
+            roles << Role.find_by({abbreviation: "LS4"})
+        end
+        if (remaining_players >= 7)
+            roles << Role.find_by({abbreviation: "LS5"})
+        end
+        if (remaining_players >= 8)
+            roles << Role.find_by({abbreviation: "MM3"})
+        end
+
+        render json: roles.shuffle
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Message.delete_all
-Game.delete_all
+#Message.delete_all
+#Game.delete_all
 #User.delete_all
+Role.delete_all
 
 
-Game.create([{name: "GAME 1", num_of_players: 5, status: "WAITING"}, {name: "GAME 2", num_of_players: 10, status: "IN_PROGRESS"}, {name: "GAME 3", num_of_players: 7, status: "FINISHED"} ])
-User.create([{username: "AA", password: "123"},{username: "BB", password: "123"},{username: "CC", password: "123"},{username: "DD", password: "123"},{username: "EE", password: "123"}])
+#Game.create([{name: "GAME 1", num_of_players: 5, status: "WAITING"}, {name: "GAME 2", num_of_players: 10, status: "IN_PROGRESS"}, {name: "GAME 3", num_of_players: 7, status: "FINISHED"} ])
+#User.create([{username: "AA", password: "123"},{username: "BB", password: "123"},{username: "CC", password: "123"},{username: "DD", password: "123"},{username: "EE", password: "123"}])
 
 Role.create([
-    {name: "Merlin", faction: "good", abbreviation: "MER", img_url: "https://gamepedia.cursecdn.com/eternitywiki/e/e0/Pallegina-portrait.jpg?version=d7ecf31cae0c64e683894dda431260f4"}, {name: "Percival", faction: "good", abbreviation: "PER", img_url: "https://gamepedia.cursecdn.com/eternitywiki/4/41/Human_male_PoE1_portrait_07_lg.png?version=a3cac8a8fc8a7a52876d420d047ff03b"}, {name: "Loyal Servant of Author", faction: "good", abbreviation: "LS1", img_url: "https://gamepedia.cursecdn.com/eternitywiki/9/90/Aumaua_female_PoE1_portrait_1_lg.png?version=cdd740cd7a3c5d41553662756c5139ad"},
+    {name: "Merlin Knows Evil", faction: "good", abbreviation: "MER", img_url: "https://gamepedia.cursecdn.com/eternitywiki/e/e0/Pallegina-portrait.jpg?version=d7ecf31cae0c64e683894dda431260f4"}, {name: "Percival", faction: "good", abbreviation: "PER", img_url: "https://gamepedia.cursecdn.com/eternitywiki/4/41/Human_male_PoE1_portrait_07_lg.png?version=a3cac8a8fc8a7a52876d420d047ff03b"}, 
+    
+    {name: "Loyal Servant of Author", faction: "good", abbreviation: "LS1", img_url: "https://gamepedia.cursecdn.com/eternitywiki/9/90/Aumaua_female_PoE1_portrait_1_lg.png?version=cdd740cd7a3c5d41553662756c5139ad"},
 
     {name: "Loyal Servant of Author", faction: "good", abbreviation: "LS2", img_url: "https://gamepedia.cursecdn.com/eternitywiki/c/cc/Dwarf_female_PoE1_portrait_2_lg.png?version=7a1139432e6758a893d89417b2eee5df"},
 
@@ -24,7 +27,7 @@ Role.create([
 
     {name: "Loyal Servant of Author", faction: "good", abbreviation: "LS5", img_url: "https://gamepedia.cursecdn.com/eternitywiki/1/1a/Dwarf_male_PoE1_portrait_1_lg.png?version=f058f4663b605b4a6070bdbb82227134"},
 
-    {name: "Assassin", faction: "bad", abbreviation: "ASS", img_url: "https://gamepedia.cursecdn.com/eternitywiki/9/9a/Elf_female_PoE1_portrait_3_lg.png?version=832bbf21c1d3f0bf0b5ca81ad984151f"},
+    {name: "Assassin Kills Merlin", faction: "bad", abbreviation: "ASS", img_url: "https://gamepedia.cursecdn.com/eternitywiki/9/9a/Elf_female_PoE1_portrait_3_lg.png?version=832bbf21c1d3f0bf0b5ca81ad984151f"},
 
     {name: "Minion of Mordred", faction: "bad", abbreviation: "MM1", img_url: "https://gamepedia.cursecdn.com/eternitywiki/a/a8/Godlike_male_PoE1_portrait_3_lg.png?version=a5823b176924282d6dd3838f2f7a5414"},
 
