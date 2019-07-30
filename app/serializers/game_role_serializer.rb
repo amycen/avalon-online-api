4cf4f1c39754cefbdd47ee94a161e00175284df1
Amy Cen <cenamy@gmail.com>
2019-07-30 14:32:07 -0400

    added gr serializer

--- app/serializers/game_role_serializer.rb
+++ app/serializers/game_role_serializer.rb
@@ -1,3 +1,9 @@
 class GameRoleSerializer < ActiveModel::Serializer
-  attributes :id, :game_id, :user_id, :role_id, :result
+  attributes :id, :game_id, :user_id, :role_id, :result, :username
+  belongs_to :user
+
+  def username
+    object.user.username
+  end
+
 end
