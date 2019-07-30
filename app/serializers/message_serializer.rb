703664fd5ccaf66f57fdf5d6920487f4f5119c4f
Amy Cen <cenamy@gmail.com>
2019-07-30 14:11:15 -0400

    added username to msg

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,8 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :game_id, :user_id, :text, :created_at
+  attributes :id, :game_id, :user_id, :text, :created_at, :username
+  belongs_to :user
+
+  def username
+    object.user.username
+  end
 end
